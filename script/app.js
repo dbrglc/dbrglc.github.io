1fcee2e1741e5c149c26d301c3611ffe4f9374c0
Gianluca D'Abrosca <gianluca.dabrosca.1999@gmail.com>
2021-10-30 14:33:18 +0200

    github card improve

--- script/app.js
+++ script/app.js
@@ -1,25 +1,23 @@
-// Function for generate the cards of the projects
+// Generate the cards of the projects
 let repos = async function () {
     let fetchCall = await fetch("https://api.github.com/users/dbrglc/repos");
     let result = await fetchCall.json();
-    let initailTemplate = "";
+    let template = "";
     for (let i = 0; i < result.length; i++) {
-      const template = `
-      <div class="project-card">
-        <h4><a href="${result[i].html_url}"> ${(result[i].name).replace(/-/g,' ')} </a></h4>
-        <p class="project-description">${result[i].description}</p>
-        <p class="project-language">${(result[i].language == null ? "" : result[i].language)}</p>
-      </div>`;
-      initailTemplate += template;
+        template += `
+        <a class="project-card center" href="${result[i].html_url}">
+            <span class="title-card"> ${(result[i].name).replace(/-/g,' ')} </span>
+            <span class="project-description">${result[i].description} </span>
+        </a>`;
     }
-    document.getElementById('projects').innerHTML = initailTemplate;
-  };
+    document.getElementById('projects').innerHTML = template;
+};
   
-  // This function controls if there is the div di="projects", so then calls "repos"
-  function clickOnLink () { 
+// Check if there is the div with id='projects' before calling repos
+function clickOnLink () { 
     if (document.getElementById('projects')) {
-         repos();
+        repos();
     }
-  };
+};
   
-  clickOnLink();
+clickOnLink();
